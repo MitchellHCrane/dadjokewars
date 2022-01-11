eb6c9ba1fd8831fe6b509b6de9542e1a624f19c4
Skyler Crane <skylercrane7@gmail.com>
2022-01-11 16:26:11 -0700

    Updated app.tsx routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+
+import "./App.css";
+import "./features/intro/Intro.css";
+
 import Header from "./features/header/Header";
 import Intro from "./features/intro/Intro";
 import HowItWorks from "./features/how_it_works/HowItWorks";
@@ -5,19 +11,15 @@ import GameSetup from "./features/game_setup/GameSetup";
 import PlayerNames from "./features/player_names/PlayerNames";
 import PlayGame from "./features/play_game/PlayGame";
 import GameOver from "./features/game_over/GameOver";
-import "./App.css";
-import "./features/intro/Intro.css";
-import { BrowserRouter as Router } from "react-router-dom";
 import PageNotFound from "./features/not_found/NotFound";
-import { Route, Routes, Switch } from "react-router-dom";
+import React from "react";
 
 function App() {
   return (
-    <>
+     <React.Fragment>
       <Header />
       <Router>
         <Routes>
-          <switch>
             <Route path="/" element={<Intro />} />
             <Route path="/how-it-works" element={<HowItWorks />} />
             <Route path="/game-setup" element={<GameSetup />} />
@@ -26,11 +28,10 @@ function App() {
             <Route path="/game-over" element={<GameOver />} />
             {/* <Route path="/sudden-death-game-play" element={SuddenDeathGamePlay} /> */}
             {/* <Route path="/sudden-death" element={SuddenDeath /> */}
-            <Route component={PageNotFound} />
-          </switch>
+            <Route path="/*" element={<PageNotFound />} />
         </Routes>
       </Router>
-    </>
+    </React.Fragment>
   );
 }
 
